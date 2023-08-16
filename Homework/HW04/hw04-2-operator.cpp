aec381077d33fc3b9425e4d2dc4c58e118b63ffa
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-16 23:06:12 +0700

    update hw4-2

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -1,31 +1,30 @@
 /*
     จงเติมโค้ดโปรแกรมตามที่แสดงในส่วนของ Placeholder เพื่อแสดงผลลัพธ์ของ Operator ด้านซ้ายมือ พร้อมทั้งแก้ไขโค้ดโปรแกรมให้ถูกต้อง
 */
-#include <stdio.h> // Corrected header name
-
+#include <stdio.h>
 int main() {
     int a = 0, b = 0 ;
     printf( "Please enter value (a b) : " ) ;
-    scanf( "%d %d", &a, &b ); // Corrected input statement
+    scanf( "%d %d", &a, &b  ) ; //TODO: #11 Input variable 'a' and 'b'
 
-    // Completed and corrected operator outputs
+    //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
     printf( "a = %d , b = %d \n", a, b ) ;
-    printf( "a + b = %d \n", a + b ) ;
-    printf( "a - b = %d \n", a - b ) ;
-    printf( "a * b = %d \n", a * b ) ; // Added missing semicolon
-    printf( "a / b = %lf (b must be greater than 0)\n", b > 0 ? (double)a / b : 0.0 ) ;
-    printf( "a %% b = %d \n", a % b ) ; // Escaped '%' with '%%'
-    printf( "++a = %d \n", ++a ) ;
-    printf( "a-- = %d \n", a-- ) ;
-    printf( "a += 1 %d \n", a += 1 ) ;
-    printf( "a += b %d \n", a += b ) ;
-    printf( "a -= 1 %d \n", a -= 1 ) ; // Changed ':' to ';' to fix syntax error
-    printf( "a -= b %d \n", a -= b ) ;
-    printf( "a *= 1 %d \n", a *= 1 ) ;
-    printf( "a *= b %d \n", a *= b ) ;
-    printf( "a %%= 1 %d \n", a %= 1 ) ; // Changed '%' to '%%'
-    printf( "a %%= b %d \n", a %= b ) ; // Changed '%' to '%%'
-    printf( "a && b %d \n", a && b ) ;
-    printf( "a || b %d \n", a || b ) ;
+    printf( "a + b = %d\n", a + b ) ;
+    printf( "a - b = %d\n", a - b ) ;
+    printf( "a * b = %d\n", a * b ) ;
+    printf( "a / b = %d (b must greater than 0)\n", b > 0 ? a : 0 ) ;
+    printf( "a %% b = %d\n", a % b ) ;
+    printf( "++a = %d\n", ++a ) ;
+    printf( "a-- = %d\n", a-- ) ;
+    printf( "a += 1 %d\n", a += 1 ) ;
+    printf( "a += b %d\n", a += b) ;
+    printf( "a -= 1 %d\n", a -= 1 ) ;
+    printf( "a -= b %d\n", a -= b ) ;
+    printf( "a *= 1 %d\n", a *= 1 ) ;
+    printf ( "a *= b %d\n", a *= b ) ;
+    printf( "a %%= 1 %d\n", a %= 1 ) ;
+    printf( "a %%= b %d\n", a %= b ) ;
+    printf( "a && b %d\n", a && b ) ;
+    printf( "a || b %d\n", a || b ) ;
     return 0 ;
 }//end function
